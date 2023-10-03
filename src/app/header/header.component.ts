5f5f5649da53c2550eb54f45cbb7258dc23814d0
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-10-03 13:50:09 +0200

    added hamburger-menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,8 +7,27 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   activeLink: string = '';
-  
+  images = [
+    './assets/img/icons/burger-menu.png',
+    './assets/img/icons/hamburger-menu-transition.png',
+    './assets/img/icons/close-menu-transition.png',
+    './assets/img/icons/close-menu.png'
+  ];
+  menuIsOpen:boolean = false;
 
+  toggleMenu() {
+    if (!this.menuIsOpen) {
+      this.menuIsOpen = true;
+      console.log('menu was opend');
+ 
+    } else if (this.menuIsOpen) {
+      this.menuIsOpen = false;
+      console.log('menu was closed');
+    }
+  }
+
+
+  
   setActiveLink(linkName: string) {
     this.activeLink = linkName;
   }
