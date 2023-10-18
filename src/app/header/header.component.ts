dd610ee9decac947924b0090d624ad26e6ca755d
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-10-18 21:08:03 +0200

    added mobile menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,27 +7,20 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
   activeLink: string = '';
-  images = [
-    './assets/img/icons/burger-menu.png',
-    './assets/img/icons/hamburger-menu-transition.png',
-    './assets/img/icons/close-menu-transition.png',
-    './assets/img/icons/close-menu.png'
-  ];
-  menuIsOpen:boolean = false;
+  menuIsOpen: boolean = false;
 
   toggleMenu() {
     if (!this.menuIsOpen) {
       this.menuIsOpen = true;
-      console.log('menu was opend');
- 
     } else if (this.menuIsOpen) {
       this.menuIsOpen = false;
-      console.log('menu was closed');
     }
   }
 
+  closeMenu() {
+    this.menuIsOpen = false;
+  }
 
-  
   setActiveLink(linkName: string) {
     this.activeLink = linkName;
   }
