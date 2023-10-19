97ca51c5bd31164497e12e83843e2ce5be373bb0
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-10-19 21:31:54 +0200

    form validation and rework for error messages

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,13 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  isFormValid = false;
+  user = {
+    name: '',
+    email: '',
+    message: '',
+    privacyPolicy: false
+  };
 
   @ViewChild('myForm') myForm!: ElementRef;
   @ViewChild('nameField') nameField!: ElementRef;
@@ -14,32 +21,36 @@ export class ContactFormComponent {
   @ViewChild('sendButton') sendButton!: ElementRef;
 
   async sendMail() {
-    console.log('Sending mail', this.myForm);
-    let nameField =  this.nameField.nativeElement
-    let messageField = this.messageField.nativeElement
-    let emailField = this.emailField.nativeElement
-    let sendButton = this.sendButton.nativeElement
-    nameField.disabled = true;
-    messageField.disabled = true;
-    emailField.disabled = true;
-    sendButton.disabled = true;
+
+    console.log(this.user.name, this.user.email, this.user.message, this.user.privacyPolicy);
+    
+
+    // console.log('Sending mail', this.myForm);
+    // let nameField =  this.nameField.nativeElement
+    // let messageField = this.messageField.nativeElement
+    // let emailField = this.emailField.nativeElement
+    // let sendButton = this.sendButton.nativeElement
+    // nameField.disabled = true;
+    // messageField.disabled = true;
+    // emailField.disabled = true;
+    // sendButton.disabled = true;
     //ANIMATION anzeigen
 
-    let fd = new FormData();
-    fd.append('name', nameField.value);
-    fd.append('message', messageField.value);
-    await fetch('https://rafael-tauschek.de/send_mail/send_mail.php',
-     {
-      method: 'POST',
-      body: fd
-     }
-    ),
+    // let fd = new FormData();
+    // fd.append('name', nameField.value);
+    // fd.append('message', messageField.value);
+    // await fetch('https://rafael-tauschek.de/send_mail/send_mail.php',
+    //  {
+    //   method: 'POST',
+    //   body: fd
+    //  }
+    // ),
 
-    //Text anzeigen, Nachricht gesendet!
-    nameField.disabled = false;
-    messageField.disabled = false;
-    emailField.disabled = false;
-    sendButton.disabled = false;
+    // //Text anzeigen, Nachricht gesendet!
+    // nameField.disabled = false;
+    // messageField.disabled = false;
+    // emailField.disabled = false;
+    // sendButton.disabled = false;
   }
 
 
