7ef919e5a7d7c4ac74f5be4e693ab05fb2f29ad8
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-10-26 21:32:30 +0200

    some changes

--- src/app/about-me-section/about-me-section.component.ts
+++ src/app/about-me-section/about-me-section.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-about-me-section',
   templateUrl: './about-me-section.component.html',
-  styleUrls: ['./about-me-section.component.scss']
+  styleUrls: ['./about-me-section.component.scss'],
 })
+
 export class AboutMeSectionComponent {
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatDialogModule } from '@angular/material/dialog';
 
+
 @NgModule({
   declarations: [
     AppComponent,
