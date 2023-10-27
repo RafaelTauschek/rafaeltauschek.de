a716ec9732415cf12c10651788ff9ddc2d80b091
Rafael Tauschek <rafael.tauschek@gmail.com>
2023-10-27 19:47:51 +0200

    added ts for pf

--- src/app/portfolio-section/portfolio-section.component.ts
+++ src/app/portfolio-section/portfolio-section.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class PortfolioSectionComponent {
 
+  projects = [
+    {
+      title: 'Join',
+      technologies: ['JavaScript', 'HTML', 'CSS', 'GroupProject'],
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      img: './assets/img/portfolio/join-big.png',
+      githubLink: 'https://github.com/RafaelTauschek/Join',
+      livetestLink: 'https://join.rafael-tauschek.de'
+    },
+    {
+      title: 'El Pollo Loco',
+      technologies: ['JavaScript', 'HTML', 'CSS'],
+      description: 'Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.',
+      img: './assets/img/portfolio/el-pollo-loco-big.png',
+      githubLink: 'https://github.com/RafaelTauschek/El-Pollo-Loco',
+      livetestLink: 'https://el-pollo-loco.rafael-tauschek.de'
+    }
+  ]
+
+
 }
